04176b7d97c0088ec5aa34440953c14105cb01fc
shawn220a <shawn.42496@gmail.com>
2023-01-27 20:11:40 -0600

    Add about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,15 @@ export default class About extends Component {
   render() {
     return (
       <section id='About'>
-        <div></div>
+        <div>
+          <p>
+            {' '}
+            My name is David Edelson I am currently enrolled with the University
+            of Texas computer coding class .I am recently retired with the
+            United States Coast Guard. I enjoy working wiht JAVA, REACT, HTML
+            and CSS.
+          </p>
+        </div>
       </section>
     );
   }
