4c85d4364b0c7bb45cd1cd7343e44a0bdc4df29b
shawn220a <shawn.42496@gmail.com>
2023-01-27 20:25:47 -0600

    Update portfolio page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import job from '../images/jobb.png';
+import tech from '../images/tech-blog.png';
 
 export default class Projects extends Component {
   render() {
@@ -6,23 +8,33 @@ export default class Projects extends Component {
       <div>
         Projects
         <section id='Project'>
-          <h2 class='container'>Portfolio</h2>
-          <div class='container3'>
-            {/* <div class="container3">
-                <a href="http://stormy-cliffs-65992.herokuapp.com/" target="blank"><img class="img"  width="200px" height="200px" alt="project 1"></img></a>
-                <p class="cap">Jays on your feet Project 3</p>
-                
+          <h2 className='container'>Portfolio</h2>
+          <div className='container3'>
+            <div className='container3'>
+              <a
+                href='https://github.com/medeldiego47/job_board'
+                target='blank'
+              >
+                <img
+                  src={job}
+                  width='200px'
+                  height='200px'
+                  alt='project 1'
+                ></img>
+              </a>
+              <p class='cap'>Grab-A-Job</p>
             </div>
-            <div class="container3">
-                <a href="https://catpolk.github.io/Bored-NO-MO-project-1/"><img width="200px" height="200px" alt="project 1"></img></a>
-                <p class="cap">Bored No Mo</p>
-                
+            <div class='container3'>
+              <a href='https://github.com/DBEdelson/Tech-Blog-' target='blank'>
+                <img
+                  src={tech}
+                  width='200px'
+                  height='200px'
+                  alt='project 1'
+                ></img>
+              </a>
+              <p className='cap'>Tech Blog</p>
             </div>
-            <div class="container3">
-                <a href="https://grabajob.herokuapp.com/"><img  width="200px" height="200px" alt="project 2"></img></a>
-
-                <p class="cap">GrabAJob</p>
-            </div> */}
           </div>
         </section>
       </div>
